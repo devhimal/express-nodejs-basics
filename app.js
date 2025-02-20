08db7709a6beaf5ddeab9374ac02ab5183dc32a9
devhimal <himalhobbs.364@gmail.com>
2025-02-20 13:37:08 +0545

    feat: added server error handlers

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const mongodb = process.env.MONGODB_CONNECTION_STRING;
 const authJwt = require("./helpers/jwt")
+const errorHandler = require("./helpers/error.handler")
 
 const productRouter = require("./routers/product.router")
 const categoryRouter = require("./routers/category.router")
@@ -15,7 +16,10 @@ const userRouter = require("./routers/users.router")
 // Middleware
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
-app.use(authJwt())
+app.use(authJwt());
+
+// handling the server error
+app.use(errorHandler)
 
 // Routes
 app.use('/api/products', productRouter);
---
+++ helpers/error.handler.js
@@ -0,0 +1,17 @@
+function errorHandler(err, req, res, next) {
+
+  // here we can specify what kind of message you would like to give 
+  //for the certail errors: like if you like to give a specific error message
+  // for authentication and permission error you could write it here, just by 
+  // checking if err.name ==  "AuthorizationError"
+
+  if (err) {
+    return res.status(500).json({
+      message: "Server error",
+      error: err
+    })
+  }
+}
+
+
+module.exports = errorHandler
