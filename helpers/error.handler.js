ab6b624b65a5c3d3a3c7e81f107212a827528857
devhimal <himalhobbs.364@gmail.com>
2025-02-20 14:14:04 +0545

    feat: user athentication added for each route

--- helpers/error.handler.js
+++ helpers/error.handler.js
@@ -4,12 +4,17 @@ function errorHandler(err, req, res, next) {
   //for the certail errors: like if you like to give a specific error message
   // for authentication and permission error you could write it here, just by 
   // checking if err.name ==  "AuthorizationError"
-
-  if (err) {
+  if (err.name == "UnauthorizedError") {
     return res.status(500).json({
-      message: "Server error",
+      message: "You are not authorized to access it.",
       error: err
     })
+  }
+  else {
+    return next(res.status(500).json({
+      message: "Server error",
+      error: err
+    }))
   }
 }
 
--- helpers/jwt.js
+++ helpers/jwt.js
@@ -6,6 +6,16 @@ function authJwt() {
   return expressJwt({
     secret,
     algorithms: ['HS256']
+  }).unless({
+    path: [
+      {
+        url: /\/api\/products(.*)/, method: ['GET', 'OPTIONS'] // here we are authorizing only GET method for /products without need of token.
+      },
+      {
+        url: /\/api\/categories(.*)/, method: ['GET', 'OPTIONS'] // here we are authorizing only GET method for /products without need of token.
+      },
+      '/api/users/login',
+    ]
   })
 }
 
